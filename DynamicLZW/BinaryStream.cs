0044650fd09c4aa7021561e7424f2448fd8fe491
Quajak <jasper.gerigk@gmx.net>
2021-09-11 11:52:47 +0200

    Add project files.

---
+++ DynamicLZW/BinaryStream.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+
+namespace DynamicLZW
+{
+    /// <summary>
+    /// This class allows reading from a byte array single bits
+    /// </summary>
+    public class BinaryStream : Stream
+    {
+        private byte[] data;
+        private long bytePos;
+        private byte bitPos;
+
+        public BinaryStream(byte[] Data)
+        {
+            data = Data;
+            bytePos = 0;
+            bitPos = 0;
+        }
+
+        public override bool CanRead => data.Length != bytePos;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => false;
+
+        /// <summary>
+        /// Length in bits
+        /// </summary>
+        public override long Length => data.Length * 8;
+
+        public override long Position
+        {
+            get => bytePos * 8 + bitPos;
+            set
+            {
+                bitPos = (byte)(value % 8);
+                bytePos = value / 8;
+            }
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+        /// <summary>
+        /// Will read count number of bits. The first byte will be filled from LSB first. So the first byte migth not be fully filled but all
+        /// later ones will be
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count">Number of bits to read</param>
+        /// <returns>Number of bytes read rounded up</returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (!CanRead)
+            {
+                return 0;
+            }
+            var bytesToRead = (int)Math.Min(buffer.Length - offset, data.Length - bytePos);
+            var bitsToRead = Math.Min(Math.Min(bytesToRead * 8, Length - bytePos * 8 - bitPos), count);
+            buffer[offset] = 0;
+            for (int i = 0; i < bitsToRead % 8; i++)
+            {
+                buffer[offset] <<= 1;
+                buffer[offset] |= ReadBit();
+            }
+            if(bitsToRead % 8 != 0)
+            {
+                offset++;
+            }
+            for (int i = 0; i < bitsToRead / 8; i++)
+            {
+                buffer[offset++] = ReadByte();
+            }
+
+
+            return bytesToRead;
+        }
+
+        /// <summary>
+        /// Read one byte from the stream
+        /// </summary>
+        /// <returns></returns>
+        private byte ReadByte()
+        {
+            if(bitPos == 0)
+            {
+                return data[bytePos++];
+            }
+            else
+            {
+                byte val = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    val <<= 1;
+                    val |= ReadBit();
+                }
+                return val;
+            }
+        }
+
+        /// <summary>
+        /// Read one bit from the stream
+        /// </summary>
+        /// <returns></returns>
+        private byte ReadBit()
+        {
+            if (!CanRead)
+            {
+                throw new IOException();
+            }
+            var readByte = data[bytePos];
+            byte bit = (byte)((readByte & (1 << (7 - bitPos))) >> (7 - bitPos));
+            bitPos++;
+            if(bitPos == 8)
+            {
+                bitPos = 0;
+                bytePos++;
+            }
+            return bit;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    bytePos = offset / 8;
+                    bitPos = (byte)(offset % 8);
+                    break;
+                case SeekOrigin.Current:
+                    bytePos += offset / 8;
+                    bitPos += (byte)(offset % 8);
+                    break;
+                case SeekOrigin.End:
+                    bytePos = Length - offset / 8;
+                    bitPos = (byte)(Length - offset % 8);
+                    break;
+                default:
+                    break;
+            }
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ DynamicLZWTests/BinaryStreamTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using DynamicLZW;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicLZW.Tests
+{
+    [TestFixture()]
+    public class BinaryStreamTests
+    {
+        [Test()]
+        public void BinaryStreamTest()
+        {
+            byte[] data = new byte[] { 0x1, 0x2, 0x3 };
+            BinaryStream binaryStream = new BinaryStream(data);
+            Assert.AreEqual(24, binaryStream.Length);
+            Assert.AreEqual(0, binaryStream.Position);
+            Assert.IsTrue(binaryStream.CanRead);
+            Assert.IsTrue(binaryStream.CanSeek);
+            Assert.IsFalse(binaryStream.CanWrite);
+        }
+
+         [Test()]
+         public void TestSimpleRead()
+        {
+            byte[] data = new byte[] { 0b10101111, 0b00001100 };
+            var binaryStream = new BinaryStream(data);
+            byte[] output = new byte[1];
+            binaryStream.Read(output, 0, 4);
+            Assert.AreEqual(0b1010, output[0]);
+            binaryStream.Read(output, 0, 4);
+            Assert.AreEqual(0b1111, output[0]);
+            binaryStream.Read(output, 0, 3);
+            Assert.AreEqual(0, output[0]);
+            binaryStream.Read(output, 0, 5);
+            Assert.AreEqual(0b1100, output[0]);
+        }
+
+        [Test()]
+        public void TestLongRead()
+        {
+            byte[] data = new byte[] { 0b10101111, 0b10110011 };
+            var binaryStream = new BinaryStream(data);
+            byte[] output = new byte[2];
+            binaryStream.Read(output, 0, 14);
+            Assert.AreEqual(0b101011, output[0]);
+            Assert.AreEqual(0b11101100, output[1]);
+        }
+
+        [Test()]
+        public void TestFullRead()
+        {
+            byte[] data = new byte[] { 0b10101111, 0b10110011 };
+            var binaryStream = new BinaryStream(data);
+            byte[] output = new byte[2];
+            binaryStream.Read(output, 0, 16);
+            Assert.AreEqual(0b10101111, output[0]);
+            Assert.AreEqual(0b10110011, output[1]);
+        }
+
+    }
+}
